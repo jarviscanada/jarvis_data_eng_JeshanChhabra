a6192992bb06632dd33f5873ef7b6a0becfc6501
jeshan26 <jeshanchhabra@yahoo.com>
2022-05-31 17:18:50 -0400

    Added regex class

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/HelloWorld.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/HelloWorld.java
@@ -5,6 +5,8 @@ class HelloWorld {
     // Your program begins with a call to main().
     // Prints "Hello, World" to the terminal window.
     public static void main(String args[]) {
+        RegexExcImp test = new RegexExcImp();
+        System.out.println(test.matchJpeg(".jpeg"));
         System.out.println("Hello, World");
     }
 }
