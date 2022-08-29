e145fcd9c2129d521d0f86e7066accc6685d6dcc
Jeshan26 <jeshanchhabra@yahoo.com>
2022-08-29 20:52:56 +0000

    init springboot

---
+++ springboot/src/main/java/ca/jrvs/apps/trading/Application.java
@@ -0,0 +1,10 @@
+package ca.jrvs.apps.trading;
+
+class HelloWorld{
+
+//program begins by calling the main method
+
+public static void main(String args[]){
+System.out.println("Hello,World");
+}
+}
